4bfb46827025ee976145a862aa74358565c6925b
Bo Dai <bo.dai@acuityads.com>
2019-03-14 17:10:49 -0400

    add route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './pages/auth/auth.dart';
+import './pages/product/products_admin.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +14,9 @@ class MyApp extends StatelessWidget {
                 accentColor: Colors.deepPurple,
             ),
             home: AuthPage(),
+            routes: {
+                'admin': (BuildContext context) => ProductsAdminPage(),
+            },
         );
     }
 }

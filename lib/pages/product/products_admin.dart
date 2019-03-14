433e735aca3c914bd5647fa381b1a73a72a0712e
Bo Dai <bo.dai@acuityads.com>
2019-03-14 15:17:01 -0400

    add tabs

--- lib/pages/product/products_admin.dart
+++ lib/pages/product/products_admin.dart
@@ -1,33 +1,53 @@
 import 'package:flutter/material.dart';
 import '../product/products_page.dart';
+import '../product/product_create.dart';
+import '../product/product_list.dart';
 
 class ProductsAdminPage extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-        return Scaffold(
-            drawer: Drawer(
-                child: Column(
+        return DefaultTabController(
+            length: 2,
+            child: Scaffold(
+                drawer: Drawer(
+                    child: Column(
+                        children: <Widget>[
+                            AppBar(
+                                automaticallyImplyLeading: false,
+                                title: Text('Choose'),
+                            ),
+                            ListTile(
+                                title: Text('All Products'),
+                                onTap: () {
+                                    Navigator.pushReplacement(context, MaterialPageRoute(
+                                        builder: (BuildContext context) => ProductsPage()
+                                    ));
+                                },
+                            ),
+                        ],
+                    ),
+                ),
+                appBar: AppBar(
+                    title: Text('Manage Products'),
+                    bottom:TabBar(
+                        tabs: <Widget>[
+                            Tab(
+                                text: 'Create Product',
+                                icon: Icon(Icons.create),
+                            ),
+                            Tab(
+                                text: 'My Products',
+                                icon: Icon(Icons.list),
+                            ),
+                        ],
+                    ),
+                ),
+                body: TabBarView(
                     children: <Widget>[
-                        AppBar(
-                            automaticallyImplyLeading: false,
-                            title: Text('Choose'),
-                        ),
-                        ListTile(
-                            title: Text('All Products'),
-                            onTap: () {
-                                Navigator.pushReplacement(context, MaterialPageRoute(
-                                    builder: (BuildContext context) => ProductsPage()
-                                ));
-                            },
-                        ),
+                        ProductCreatePage(),
+                        ProductListPage(),
                     ],
                 ),
-            ),
-            appBar: AppBar(
-                title: Text('Manage Products'),
-            ),
-            body: Center(
-                child: Text('Manage Products'),
             ),
         );
     }

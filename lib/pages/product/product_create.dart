98498ec3af64dfe81dc65eabc0ce546b55a15a65
daibo <bobriandai@gmail.com>
2019-03-15 00:18:46 -0400

    add modal and dialog

--- lib/pages/product/product_create.dart
+++ lib/pages/product/product_create.dart
@@ -4,7 +4,19 @@ class ProductCreatePage extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return Center(
-            child: Text('Create a Products'),
+            child: RaisedButton(
+                child: Text('Save'),
+                onPressed: () {
+                    showBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                            return Center(
+                                child: Text('This is a modal'),
+                            );
+                        }
+                    );
+                },
+            ),
         );
     }
 }
--- lib/pages/product/product_page.dart
+++ lib/pages/product/product_page.dart
@@ -7,6 +7,33 @@ class ProductPage extends StatelessWidget {
 
     ProductPage(this.product);
 
+    _showWarningDialog(BuildContext context) {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+                return AlertDialog(
+                    title: Text('Are you sure?'),
+                    content: Text('This action cannot be undone!'),
+                    actions: <Widget>[
+                        FlatButton(
+                            child: Text('DISCARD'),
+                            onPressed: () {
+                                Navigator.pop(context);
+                            },
+                        ),
+                        FlatButton(
+                            child: Text('CONTINUE'),
+                            onPressed: () {
+                                Navigator.pop(context);
+                                Navigator.pop(context, true);
+                            },
+                        ),
+                    ],
+                );
+            }
+        );
+    }
+
     @override
     Widget build(BuildContext context) {
         return WillPopScope(
@@ -38,7 +65,7 @@ class ProductPage extends StatelessWidget {
                             RaisedButton(
                                 child: Text('Delete'),
                                 color: Theme.of(context).accentColor,
-                                onPressed: () => Navigator.pop(context, true),
+                                onPressed: () => _showWarningDialog(context),
                             ),
                         ],
                     )

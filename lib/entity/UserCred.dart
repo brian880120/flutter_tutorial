c3f20318599c50d6b134123a9d053a06ea2ed4f8
daibo <bobriandai@gmail.com>
2019-03-21 00:25:29 -0400

    no error version

--- lib/entity/UserCred.dart
+++
@@ -1,7 +0,0 @@
-class UserCred {
-    String email;
-    String password;
-    bool acceptTerms;
-
-    UserCred(this.email, this.password, this.acceptTerms);
-}
--- lib/widgets/products/products.dart
+++ lib/widgets/products/products.dart
@@ -1,34 +1,31 @@
 import 'package:flutter/material.dart';
+
 import 'package:scoped_model/scoped_model.dart';
-import '../../widgets/products/product_card.dart';
-import '../../scoped-models/products.dart';
-import '../../entity/Product.dart';
+
+import './product_card.dart';
+import '../../models/product.dart';
+import '../../scoped-models/main.dart';
 
 class Products extends StatelessWidget {
-
-    Widget _buildProductList(List<Product> products) {
-        Widget productCards = Center(
-            child: Text('No products found, Please add some'),
-        );
-
-        if (products.length > 0) {
-            productCards = ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                    return ProductCard(products[index], index);
-                },
-                itemCount: products.length,
-            );
-        }
-
-        return productCards;
+  Widget _buildProductList(List<Product> products) {
+    Widget productCards;
+    if (products.length > 0) {
+      productCards = ListView.builder(
+        itemBuilder: (BuildContext context, int index) =>
+            ProductCard(products[index], index),
+        itemCount: products.length,
+      );
+    } else {
+      productCards = Container();
     }
+    return productCards;
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        return ScopedModelDescendant<ProductsModel>(
-            builder: (BuildContext context, Widget child, ProductsModel model) {
-                return _buildProductList(model.displayedProducts);
-            },
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    print('[Products Widget] build()');
+    return ScopedModelDescendant<MainModel>(builder: (BuildContext context, Widget child, MainModel model) {
+      return  _buildProductList(model.displayedProducts);
+    },);
+  }
 }
